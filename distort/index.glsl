de511c2e631f4e8b3b744bf66d4e0ca4fa9d242f
fmsuvM <k222.tkd.fms@gmail.com>
2016-06-11 15:21:26 +0900

    add a distort video

---
+++ distort/index.glsl
@@ -0,0 +1,20 @@
+uniform sampler2D srcTex;
+uniform sampler2D disTex;
+
+uniform float amt;
+
+varying vec4 vertTexCoord;
+
+void main(){
+
+
+	vec4 distort = texture2D(disTex, vertTexCoord.st);
+
+	//use webcam color to distort texture coordinates
+	vec2 offset = vec2(distort.y - distort.x, distort.x - distort.z)*amt;
+	vec2 coord = offset+vertTexCoord.st;
+
+	vec4 tex = texture2D(srcTex, coord);
+
+	gl_FragColor = tex;
+}
